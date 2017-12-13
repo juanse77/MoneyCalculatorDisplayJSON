db1de8130dc91e06d767e1a2220ca6a7f2975be8
usuario <usuario@10.22.148.53>
2017-12-13 09:49:43 +0000

    versión 3.2

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,9 +1,14 @@
 package moneycalculator;
 
+import model.Currency;
+import model.Money;
+import model.ExchangeRate;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -17,7 +22,7 @@ public class MoneyCalculator {
     private Map<String, Currency> currencies = new HashMap<>();
     private Money money;
     private Currency currencyTo;
-    private double exchangeRate;
+    private ExchangeRate exchangeRate;
 
     public MoneyCalculator() {
         currencies.put("USD", new Currency("USD", "Dolar americano", "$"));
@@ -35,32 +40,32 @@ public class MoneyCalculator {
         Scanner scanner = new Scanner(System.in);
         double amount = Double.parseDouble(scanner.next());
 
-        System.out.println("Introduzca código divisa origen");
+        System.out.println("Introduzca codigo divisa origen");
         Currency currency = currencies.get(scanner.next().toUpperCase());
 
         money = new Money(amount, currency);
 
-        System.out.println("Introduzca código divisa destino");
+        System.out.println("Introduzca codigo divisa destino");
         currencyTo = currencies.get(scanner.next().toUpperCase());
     }
 
     private void process() throws Exception {
-        exchangeRate = getExchangeRate(money.getCurrency().getCode(), currencyTo.getCode());
+        exchangeRate = getExchangeRate(money.getCurrency(), currencyTo);
     }
 
     private void output() {
-        System.out.println(money.getAmount() + money.getCurrency().getSymbol() + " equivalen a " + money.getAmount() * exchangeRate + currencyTo.getSymbol());
+        System.out.println(money.getAmount() + money.getCurrency().getSymbol() + " equivalen a " + money.getAmount() * exchangeRate.getRate() + currencyTo.getSymbol());
     }
 
-    private static double getExchangeRate(String from, String to) throws Exception {
-        URL url = new URL("http://api.fixer.io/latest?base=" + from + "&symbols=" + to);
+    private static ExchangeRate getExchangeRate(Currency from, Currency to) throws Exception {
+        URL url = new URL("http://api.fixer.io/latest?base=" + from.getCode() + "&symbols=" + to.getCode());
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         InputStreamReader input = new InputStreamReader(connection.getInputStream());
         try (BufferedReader reader = new BufferedReader(input)) {
             String line = reader.readLine();
-            line = line.substring(line.indexOf(to) + 5, line.indexOf("}"));
-            return Double.parseDouble(line);
+            line = line.substring(line.indexOf(to.getCode()) + 5, line.indexOf("}"));
+            return new ExchangeRate(new Date(), from, to, Double.parseDouble(line));
         }
     }
 }
