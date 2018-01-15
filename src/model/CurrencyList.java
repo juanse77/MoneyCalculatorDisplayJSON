cd7b809aa8cf0e984164918a19b565d4d2b5d059
Juan Ramírez <Juan Ramrez@DESKTOP-H4LOGIE>
2018-01-15 17:10:35 +0000

    Versión 1.21

--- src/model/CurrencyList.java
+++ src/model/CurrencyList.java
@@ -11,6 +11,11 @@ public class CurrencyList {
     public CurrencyList() {
         add(new Currency("USD", "Dolar americano", "$"));
         add(new Currency("EUR", "Euro", "€"));
+        add(new Currency("BRL", "Real brasileño", ""));
+        add(new Currency("CAD", "Dolar canadiense", ""));
+        add(new Currency("CHF", "Franco suizo", ""));
+        add(new Currency("CNY", "Yuan chino", ""));
+        add(new Currency("JPY", "Yen", ""));
     }
 
     private void add(Currency currency) {
